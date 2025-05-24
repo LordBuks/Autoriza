ffdcfa18547b3679bd4a21a9f095574def154f66
LordBuks <lords.buks@gmail.com>
2025-05-24 11:05:00 -0300

    login

--- Sistema de autorizaðùÐäes V.3/js/firebase-config.js
+++ Sistema de autorizaðùÐäes V.3/js/firebase-config.js
@@ -12,8 +12,9 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Comentando as linhas abaixo que estavam causando erro
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 
 // Inicializar Firebase
