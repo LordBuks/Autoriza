ebafa6bf953e4bef63c45e01e318d9c432725421
LordBuks <lords.buks@gmail.com>
2025-05-24 15:55:32 -0300

    aquiagora

--- Sistema de autorizaðùÐäes V.3/public/js/firebase-config.js
+++ Sistema de autorizaðùÐäes V.3/public/js/firebase-config.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Configuração do Firebase para o Sistema de Autorizações
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Inicialização do Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBWP6QblKqmOKFWqDJZEmWR7hhJ-2GEPr4",
   authDomain: "sistema-de-autorizacoes.firebaseapp.com",
@@ -17,8 +12,10 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Comentando as linhas abaixo que estavam causando erro
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
 
 // Inicializar Firebase
 class FirebaseService {
